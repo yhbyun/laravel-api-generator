421216989eacd626cd8f0a8176d3b61978860e37
YongHun Byun <yonghunbyun@gmail.com>
2015-07-13 00:06:56 +0900

    Generate rules

--- src/Mitul/Generator/Generators/API/APIControllerGenerator.php
+++ src/Mitul/Generator/Generators/API/APIControllerGenerator.php
@@ -25,7 +25,7 @@ class APIControllerGenerator implements GeneratorProvider
 	{
 		$templateData = $this->commandData->templatesHelper->getTemplate("Controller", "api");
 
-		$templateData = GeneratorUtils::fillTemplate($this->commandData->dynamicVars, $templateData);
+		$templateData = $this->fillTemplate($templateData);
 
 		$fileName = $this->commandData->modelName . "APIController.php";
 
@@ -37,5 +37,61 @@ class APIControllerGenerator implements GeneratorProvider
 		$this->commandData->fileHelper->writeFile($path, $templateData);
 		$this->commandData->commandObj->comment("\nAPI Controller created: ");
 		$this->commandData->commandObj->info($fileName);
+	}
+
+	private function fillTemplate($templateData)
+	{
+		$templateData = GeneratorUtils::fillTemplate($this->commandData->dynamicVars, $templateData);
+
+		$fillables = [];
+
+		foreach($this->commandData->inputFields as $field)
+		{
+			$fieldName = $field['fieldName'];
+			if ($fieldName !== 'RegDate' && $fieldName !== 'ModDate') {
+				$fillables[] = '"' . $field['fieldName'] . '"';
+			}
+		}
+
+		$tab = $this->commandData->tab;
+
+		$templateData = str_replace('$FIELDS$', implode(", ", $fillables), $templateData);
+
+		$templateData = str_replace('$RULES$', implode(",\n" . $tab . $tab . $tab, $this->generateRules()), $templateData);
+
+		return $templateData;
+	}
+
+	private function generateRules()
+	{
+		$rules = [];
+
+		foreach($this->commandData->inputFields as $field)
+		{
+			switch($field['fieldType'])
+			{
+				case 'integer':
+					$rule = '"' . $field['fieldName'] . '" => "required:integer"';
+					break;
+				case 'double':
+				case 'float':
+					$rule = '"' . $field['fieldName'] . '" => "required:numeric"';
+					break;
+				case 'string':
+				case 'char':
+				case 'text':
+					$rule = '"' . $field['fieldName'] . '" => "required"';
+					break;
+				default:
+					$rule = '';
+					break;
+			}
+
+			if (!empty($rule)) {
+				$rules[] = $rule;
+			}
+		}
+
+		return $rules;
 	}
 }
